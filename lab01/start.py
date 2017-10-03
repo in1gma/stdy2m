4782e25548e5ca86c76f5c4feab997881291d852
Max <in1gma@ya.ru>
2017-10-03 22:06:44 +0500

    xor test refactoring

--- lab01/start.py
+++ lab01/start.py
@@ -6,27 +6,33 @@ from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.datasets import SupervisedDataSet
 from pybrain.structure import TanhLayer
 
-net = buildNetwork(2, 2, 1, bias=True, hiddenclass=TanhLayer)
+def test_xor_network():
+    net = buildNetwork(2, 2, 1, bias=True, hiddenclass=TanhLayer)
 
-ds = SupervisedDataSet(2, 1)
-data = [
-    [(0,0), (0,)],
-    [(0,1), (1,)],
-    [(1,0), (1,)],
-    [(1,1), (0,)],
-]
+    ds = SupervisedDataSet(2, 1)
+    data = [
+        [(0,0), (0,)],
+        [(0,1), (1,)],
+        [(1,0), (1,)],
+        [(1,1), (0,)],
+    ]
 
-for input, target in data:
-    ds.addSample(input, target)
+    for input, target in data:
+        ds.addSample(input, target)
 
-trainer = BackpropTrainer(net, ds) #, learningrate = 0.001, momentum = 0.99)
-# trainer.trainUntilConvergence()
+    trainer = BackpropTrainer(net, ds) #, learningrate = 0.001, momentum = 0.99)
+    # trainer.trainUntilConvergence()
 
-for i in xrange(500):
-    trainer.train()
+    for i in xrange(500):
+        trainer.train()
 
-test = [[0, 0], [0, 1], [1, 0], [1, 1]]
-for e in test:
-    res = net.activate(e)
-    print(res)
+    test = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    for e in test:
+        res = net.activate(e)
+        print(res)
 
+def main():
+    test_xor_network() # do smthg
+
+if __name__ == "__main__":
+    main()
