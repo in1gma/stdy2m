cdc26e24c943e58c4d2cf27fc93eb9ae226fc682
Max <in1gma@ya.ru>
2017-10-08 23:11:19 +0500

    try to brain :(

--- lab01/start.py
+++ lab01/start.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python
 
 import os
-import binascii
-# xor example test
+#import binascii
+
+#import itertools
+
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.datasets import SupervisedDataSet
-from pybrain.structure import TanhLayer
+from pybrain.datasets import ClassificationDataSet #SupervisedDataSet
+from pybrain.structure import LinearLayer #TanhLayer
 
-def test_xor_network():
-    net = buildNetwork(2, 2, 1, bias=True, hiddenclass=TanhLayer)
+import magic # for teach
+
+def network():
+    net = buildNetwork(2, 3, hiddenclass=LinerLayer)
 
     ds = SupervisedDataSet(2, 1)
-    data = [
-        [(0,0), (0,)],
-        [(0,1), (1,)],
-        [(1,0), (1,)],
-        [(1,1), (0,)],
-    ]
+    classificationDataSet(2, nb_classes=3, class_labels=['A', 'B', 'C', 'D', 'Z'])
 
     for input, target in data:
         ds.addSample(input, target)
@@ -33,23 +32,87 @@ def test_xor_network():
         res = net.activate(e)
         print(res)
 
-def read_files():
-    result = {}
-    folder = './test/'
-    bytes = 3 # 2 ~ 4
-    for filename in os.listdir(folder):
-        file = open(folder + filename, 'r')
-        signature = file.read(bytes)
-        file.close()
-        hex_signature = binascii.hexlify(signature)
-        # print('{0} {1} {2}'.format(filename, signature, hex_signature))
-        result[filename] = hex_signature
-    return result
+def build_net2(learn_data):
+    learn_data = sorted(learn_data, key=lambda x: x[1])
+    keys = []
+    groups = []
+    for key, group in itertools.groupby(learn_data, lambda x: x[0]):
+        print(key)
+        keys.append(key)
+        groups.append(list(group))
+
+    #print(keys)
+
+def build_net(nbytes, learn_data):
+    #learn_data = sorted(learn_data, key=lambda x: x[1])
+    #data_group = dict((key, list(group)) for key, group in itertools.groupby(learn_data, lambda x: x[0]))
+    #keys = [key for key, group in data_group] #BUG ~ list comprehenson remove elements!!!
+    #keys = map(lambda (key, group): key, data_group) # this TOO!!!
+
+    #keys = data_group.keys()
+    keys = []
+    for item in learn_data:
+        if item[0] not in keys:
+            keys.append(item[0])
+
+    net = buildNetwork(nbytes, len(keys), hiddenclass=LinearLayer)
+
+    ds = ClassificationDataSet(nbytes, nb_classes=len(keys), class_labels=keys)
+
+    for item in learn_data:
+        #print([keys.index(item[0])])
+        #ds.appendLinked([item[1]], [keys.index(item[0])])
+        print(item[1])
+        ds.appendLinked(item[1], [keys.index(item[0])])
+
+    ds.calculateStatistics()
+    ds._convertToOneOfMany(bounds=[0, 1])
+
+    trainer = BackpropTrainer(net, verbose=True)
+    trainer.setData(ds)
+
+    #trainer.trainUntilConvergence(maxEpochs=1000)
+    for i in xrange(4):
+        trainer.train()
+
+    return net
+
+    #for key in data_group:
+     #   print(key + ' ' + str(len(data_group[key])))
+      #  for value in data_group[key]:
+       #     print()
+        #    #ds.appendLinked(value[0], [keys.index(value[1])])
+
+def brain(net, data):
+    keys = []
+    for item in data:
+        if item[0] not in keys:
+            keys.append(item[0])
+
+    for item in data:
+        result = list(net.activate(item[1]))
+        m = max(result)
+        i = result.index(m)
+        print('out: {0} is {1}, {2}'.format(m, keys[i], item[0]))
+
+def read_file(nbytes, filename):
+    buf = open(filename, 'r').read(nbytes)
+    known_type = magic.from_buffer(buf, mime=True)
+    #return known_type, buf
+    return known_type, [ord(x) for x in buf]
+
+def read_files(nbytes, folder):
+    return map(lambda f: read_file(nbytes, folder + f), os.listdir(folder))
 
 def main():
+    nbytes = 128
     # test_xor_network() # do smthg
-    test = read_files()
-    print(test)
+    learn_data = read_files(nbytes, './learn/')
+    test_data = read_files(nbytes, './test/')
+    net = build_net(nbytes, learn_data)
+
+    brain(net, learn_data)
+    #brain(net, test_data)
 
 if __name__ == '__main__':
     main()
