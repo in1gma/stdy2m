af929650d8501198cafe4e89295f3a74a9d0c2e7
Max <in1gma@ya.ru>
2017-10-18 23:20:56 +0500

    lab02 third commit

--- lab02/start.py
+++ lab02/start.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python
+
+import os
+import sys
+
+def read_files(path):
+    for filename in os.listdir(path):
+        buf = open(os.path.join(path, filename), 'r').read()
+
+def main():
+    path = sys.argv[1]
+    read_files(path)
+
+if __name__ == '__main__':
+    main()
