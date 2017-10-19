1c7fe48ae6c96030c371be16e319868ce77281bc
Max <in1gma@ya.ru>
2017-10-19 23:54:55 +0500

    lext to plain text

--- lab02/start.py
+++ lab02/start.py
@@ -3,16 +3,19 @@
 import os
 import sys
 
+from pylatexenc.latex2text import LatexNodes2Text
+
+
 def read_files(folder):
+    tex2text = LatexNodes2Text()
     for filename in os.listdir(folder):
-        try:
-            buf = open(os.path.join(folder, filename), 'r').read()
-        except IsADirectoryError:
-            continue
+        with open(os.path.join(folder, filename), 'r+') as file:
+            text = tex2text.latex_to_text(file.read())
 
 def main():
     folder = sys.argv[1]
     read_files(folder)
+        
 
 if __name__ == '__main__':
     main()
