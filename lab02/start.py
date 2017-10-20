bf08c6d2f447aa491feca943929e1944d94aabf3
Max <in1gma@ya.ru>
2017-10-21 00:48:47 +0500

    add commented text rank sumarizer

--- lab02/start.py
+++ lab02/start.py
@@ -7,6 +7,7 @@ from pylatexenc.latex2text import LatexNodes2Text
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.summarizers.lex_rank import LexRankSummarizer as Summarizer # .lsa LsaSummarizer
+# from sumy.summarizers.text_rank import TextRankSummarizer # check rank
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
@@ -16,11 +17,14 @@ def annotate_it(folder, language, sentences_count):
     stemmer = Stemmer(language)
     summarizer = Summarizer(stemmer)
     summarizer.stop_words = get_stop_words(language)
+    # text_rank_summarizer = TextRankSummarizer(stemmer) # check rank
+    # text_rank_summarizer.stop_words = get_stop_words(language)
     for filename in os.listdir(folder):
         with open(os.path.join(folder, filename), 'r+') as file:
             text = tex2text.latex_to_text(file.read())
             parser = PlaintextParser(text, tokenizer)
             summary = summarizer(parser.document, sentences_count)
+            # rated_sentences = text_rank_summarizer.rate_sentences(parser.document) # check rank
 
 def main():
     folder = sys.argv[1]
