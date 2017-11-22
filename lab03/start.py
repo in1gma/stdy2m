21d5ef3c2d8062b392bf4c8ca91ec8959a707012
Max <in1gma@ya.ru>
2017-11-22 18:15:29 +0500

    filter stop words

--- lab03/start.py
+++ lab03/start.py
@@ -8,14 +8,17 @@ from pylatexenc.latex2text import LatexNodes2Text
 from nltk import wordpunct_tokenize
 from nltk.collocations import TrigramCollocationFinder as CollocationFinder
 from nltk.collocations import TrigramAssocMeasures as AssocMeasures
+from nltk.corpus import stopwords as Stopwords
 
 def bib_it(filename, language):
 	tex2text = LatexNodes2Text()
 	measures = AssocMeasures()
+	stopwords = Stopwords.words(language)
 	with open(filename, 'r+') as file:
 		raw = file.read()
 		text = tex2text.latex_to_text(raw)
-		finder = CollocationFinder.from_words(wordpunct_tokenize(text))#, window_size=3)
+		tokens = [x for x in wordpunct_tokenize(text) if x.lower() not in stopwords]
+		finder = CollocationFinder.from_words(tokens)#, window_size=3)
 		# finder.apply_freq_filter(1)
 		# print(finder.score_ngrams(measures.raw_freq))
 		print(finder.nbest(measures.pmi, 100)) # likelihood_ratio ~ 1000
