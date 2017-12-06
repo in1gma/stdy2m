5d0b54ac0ad315fb935faf9e11105a0f8aa623b8
in1gma <wmzhkm@gmail.com>
2017-12-06 21:02:58 +0500

    write/read tex file fix

--- lab03/start.py
+++ lab03/start.py
@@ -2,15 +2,16 @@
 
 import sys
 import os
+import re
 
-from pylatexenc.latex2text import LatexNodes2Text
-
-from nltk import wordpunct_tokenize
+from nltk import word_tokenize
 from nltk.collocations import TrigramCollocationFinder as CollocationFinder
 from nltk.collocations import TrigramAssocMeasures as AssocMeasures
 from nltk.corpus import stopwords
 from nltk.corpus import words
 from nltk.stem.wordnet import WordNetLemmatizer as Lemmatizer
+
+from six import iteritems
 
 import urllib
 from bs4 import BeautifulSoup
@@ -18,35 +19,51 @@ from bs4 import BeautifulSoup
 import time
 
 def bib_it(filename_input, filename_output, language, search):
-	tex2text = LatexNodes2Text()
 	measures = AssocMeasures()
 
 	stop_words = stopwords.words(language)
 	allow_words = words.words()
 	lemmatizer = Lemmatizer()
 
-	with open(filename_input, 'r') as file:
+	pattern = r'(\\begin{document}\n*)(.*?)(\n*\\end{document})'
+
+	with open(filename_input, 'r+') as file:
 		raw = file.read()
-		text = tex2text.latex_to_text(raw)
+		text = re.findall(pattern, raw, re.DOTALL)[0][1]
 
-		finder = CollocationFinder.from_words(wordpunct_tokenize(text))
-		finder.apply_word_filter(lambda word: len(word) < 3 or word.lower() in stop_words)
+		tokens = word_tokenize(text)
+		finder = CollocationFinder.from_words(tokens)
 
-		collocations = finder.nbest(measures.pmi, 100)
+		collocations = []
+
+		for ngram, freq in iteritems(finder.ngram_fd):
+			if (not any(len(word) < 3 or word.lower() in stop_words for word in ngram)):
+				score = finder.score_ngram(measures.pmi, *ngram)
+				# print(ngram, score)
+				# print(ngram, freq)
+
+		# collocations = finder.nbest(measures.pmi, 10)
 
 		bib_file = open(filename_output, 'w')
 
 		for collocation in collocations:
 			if any([word for word in collocation if lemmatizer.lemmatize(word.lower()) not in allow_words]):
-				string = ' '.join(collocation)
-				bibtex = search(string)
+				pass
+				# print(collocation)
+				# string = ' '.join(collocation)
+				# bibtex = "::"search(string)
 
-				bib_file.write('@comment{{{0}}}\n{1}\n'.format(string, bibtex))
+				# bib_file.write('@comment{{{0}}}\n{1}\n'.format(string, bibtex))
 
 		bib_file.close()
 
+		new = re.compile(pattern, re.DOTALL).sub('\\1 {0} \\3'.format('summary'), raw)
+		file.seek(0)
+		file.truncate()
+		file.write(new)
+
 def citeseerx_search(query):
-	time.sleep(0.5)
+	# time.sleep(0.5)
 	site = 'http://citeseerx.ist.psu.edu'
 
 	# first request
